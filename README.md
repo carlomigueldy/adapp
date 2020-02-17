de83ff68be1ced3275e78cf08b542295da586234
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-02-17 09:55:29 +0800

    project init

---
+++ README.md
@@ -0,0 +1,16 @@
+# laranuxt
+Laravel API + Nuxt js frontend boilerplate with Vuetify js + JWT auth
+
+# desc
+It comes with tymon/JWT, spatie/cors, & Vuetify js 
+
+## Installation Guide
+Hehe
+
+## Linux Issues (Ubuntu 18 04)
+<a href="https://askubuntu.com/questions/1042486/permissions-in-ubuntu-18-04" target="blank">StackOver flow ref</a>
+
+```
+sudo chown -R $USER:$USER /var/www/$YOUR_DIR_NAME/$YOUR_FILE_NAME
+sudo chmod 777 -R /var/www
+```
---
+++ api/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'AuthController@login');
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    });
+});
